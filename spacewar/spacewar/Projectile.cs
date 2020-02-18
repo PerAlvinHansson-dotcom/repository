56de9f475b72838bbd67b6f1f5ee03106e2eb675
sebastianhult-dotcom <sebastian.hult@elev.ga.ntig.se>
2020-02-18 13:18:37 +0100

    vafn händer

--- spacewar/spacewar/Projectile.cs
+++ spacewar/spacewar/Projectile.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
-public class Projectile : GameObject
+
+
+namespace spacewar
 {
-	public Projectile(Texture2D texture, Vector2 startPosition) : base (texture, startPosition)
-	{
-        
-	}
-
-    List<Projectile> projectiles;
-
-    public void Update()
+    public class Projectile : GameObject
     {
+        public Projectile(Texture2D texture, Vector2 startPosition) : base(texture, startPosition)
+        {
 
+        }
+
+        List<Projectile> projectiles;
+
+        public void Update()
+        {
+
+        }
     }
 }
