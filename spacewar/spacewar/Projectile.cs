f3cadd46dc03f67391f617059167ed45bd360d79
akbar.karimi2@elev.ga.ntig.se <akbar.karimi2@elev.ga.ntig.se>
2020-02-18 14:02:00 +0100

    yes

--- spacewar/spacewar/Projectile.cs
+++ spacewar/spacewar/Projectile.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-
 class Projectile : GameObject
 {
 	public Projectile(Texture2D texture, Vector2 startPosition) : base (texture, startPosition)
