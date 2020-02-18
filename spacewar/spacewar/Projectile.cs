c3de8d2c24e903b10eb3cdb495a352c7d0e6bbd3
Alvin Larsson <alvin.larsson1@learnet.se>
2020-02-18 13:09:50 +0100

    ofwiho gh

--- spacewar/spacewar/Projectile.cs
+++ spacewar/spacewar/Projectile.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-public class Projectile : GameObject
+class Projectile : GameObject
 {
 	public Projectile(Texture2D texture, Vector2 startPosition) : base (texture, startPosition)
 	{
