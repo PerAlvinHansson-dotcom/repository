62df1da4cf1bca77b26d0ba959dd04a1ac05b6a6
Alvin Larsson <alvin.larsson1@learnet.se>
2020-04-07 20:03:41 +0200

    två spelare

--- spacewar/spacewar/Player.cs
+++ spacewar/spacewar/Player.cs
@@ -177,7 +177,7 @@ namespace spacewar
             //skjut och skapa projectile
             if (newstate.IsKeyDown(Keys.RightControl) && oldstate.IsKeyUp(Keys.RightControl))
             {
-                projectiles.Add(new Projectile(Game1.projectileTexture1, position, angle, origin));
+                projectiles.Add(new Projectile(Game1.projectileTexture1, position, angle, origin, "laser"));
             }
             foreach (Projectile projectile in projectiles.ToArray())
             {
